b350ad0e90010564988673c28ed6dc1f91a270e9
Afril <afril@youdingapp.com>
2026-02-16 00:44:45 +0700

    Update installer script

--- installer.sh
+++ installer.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-set -e
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-bash "$SCRIPT_DIR/scripts/install-docker.sh"
-bash "$SCRIPT_DIR/scripts/create-docker-container.sh"
-(cd "$SCRIPT_DIR/scripts" && bash fix-mt5-launcher.sh)
+sh scripts/install-docker.sh
+sh scripts/create-docker-container.sh
+sh scripts/fix-mt5-launcher.sh
