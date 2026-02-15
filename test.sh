c1b39b591bca7975d2019d532abdb3425036f4ef
Afril <afril@youdingapp.com>
2026-02-16 01:51:41 +0700

    Fixed wine command

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 docker cp python/test-mt5-con.py mt5:/var/www/test-mt5-con.py
 docker exec -it mt5 bash -c "chmod +x /var/www/test-mt5-con.py"
-docker exec -it mt5 bash -c "su abc -c wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip uninstall numpy -y"
-docker exec -it mt5 bash -c "su abc -c wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install \"numpy<2\""
-docker exec -it mt5 bash -c "su abc -c wine \"C:\Program Files (x86)\Python39-32\python.exe\" /var/www/test-mt5-con.py"
-
-# su abc -c wine "C:\Program Files (x86)\Python39-32\python.exe" /var/www/test-mt5-con.py
+docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip uninstall numpy -y'"
+docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install \"numpy<2\"'"
+docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" /var/www/test-mt5-con.py'"
