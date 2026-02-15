0dadcfb1c603e303a8418094a2050805472d2f5d
Afril <afril@youdingapp.com>
2026-02-16 02:11:22 +0700

    Switch docker container

--- test-postgres.sh
+++ test-postgres.sh
@@ -1,3 +1,3 @@
-docker cp python/test-postgres-con.py mt5:/var/www/test-postgres-con.py
-docker exec -it mt5 bash -c "chmod +x /var/www/test-postgres-con.py"
-docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" /var/www/test-postgres-con.py'"
+docker cp python/test-postgres-con.py mt5_postgres:test-postgres-con.py
+docker exec -it mt5_postgres bash -c "chmod +x test-postgres-con.py"
+docker exec -it mt5_postgres bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" test-postgres-con.py'"
