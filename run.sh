205ee5724afc23f7e78fbd192628e081fd5c2bd7
Afril <afril@youdingapp.com>
2026-02-16 01:49:06 +0700

    Downgrade numpy version

--- run.sh
+++ run.sh
@@ -9,6 +9,4 @@ docker cp tools/start.sh mt5:/Metatrader/start.sh
 docker exec -it mt5 bash -c "chmod +x /Metatrader/start.sh"
 
 docker restart mt5
-
-# docker exec -it mt5 bash -c "su abc -c \"$wine_executable \\\"$wine_python\\\" /var/www/test-mt5-con.py\""
 docker logs -f mt5
