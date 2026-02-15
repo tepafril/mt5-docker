4a16cfa21b6675db05513c706475e43ddde82165
Afril <afril@youdingapp.com>
2026-02-16 01:16:18 +0700

    separate install & run shell

--- installer.sh
+++
@@ -1,3 +0,0 @@
-sh scripts/install-docker.sh
-# sh scripts/create-docker-container.sh
-# sh scripts/fix-mt5-launcher.sh
---
+++ run.sh
@@ -0,0 +1,19 @@
+mkdir -p mt5
+cp docker-compose.yaml mt5/docker-compose.yaml
+
+cd mt5
+docker compose up -d
+cd ..
+
+docker cp tools/start.sh mt5:/Metatrader/start.sh
+docker exec -it mt5 bash -c "chmod +x /Metatrader/start.sh"
+
+docker cp python/test-mt5-con.py mt5:/var/www/test-mt5-con.py
+docker exec -it mt5 bash -c "chmod +x /var/www/test-mt5-con.py"
+
+docker restart mt5
+
+sleep 30
+
+docker exec -it mt5 bash -c "su abc -c \"$wine_executable \\\"$wine_python\\\" /var/www/test-mt5-con.py\""
+docker logs -f mt5
