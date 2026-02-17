f36f1b07b614227d99e6cb89e57e663dcbb4bc5a
Afril <afril@youdingapp.com>
2026-02-17 11:52:34 +0700

    Install pandas

--- run.sh
+++ run.sh
@@ -35,9 +35,10 @@ docker restart mt5
 
 echo -e "${GREEN}[6/8] Installing numpy<2 and psycopg2-binary for MT5 (Wine Python)...${NC}"
 docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip uninstall numpy -y'"
-docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install \"numpy<2\" '"
+docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install \"numpy<2\"'"
 
 echo -e "${GREEN}[7/8] Installing numpy<2 and psycopg2-binary for MT5 (Wine Python)...${NC}"
+docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install pandas'"
 docker exec -it mt5 bash -c "su abc -c 'wine \"C:\Program Files (x86)\Python39-32\python.exe\" -m pip install psycopg2-binary --only-binary :all:'"
 
 echo -e "${GREEN}[8/8] Restarting container after pip changes...${NC}"
